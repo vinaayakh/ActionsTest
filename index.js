e7ad41aab149cb3a04ca376289a9da265cc7cc7c
vinaayakh <vinaayakh.g@aot-technologies.com>
2017-11-12 21:24:07 +0530

    third commit

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ restService.get('/', function (req, res) {
 restService.get('/events', function (req, res) {
     console.log(req);
     const app = new DialogflowApp({ req, res });
-    req.get('https://www.yepdesk.com/rest/v1/events', function (err, response, body) {
-        console.log(err);
-        console.log(response);
+    return req.get('https://www.yepdesk.com/rest/v1/events', function (err, response, body) {
+        // console.log(err);
+        // console.log(response);
         console.log(body);
-        return res.json(body);
-        app.buildCarousel()
-            .addItems([
-                app.buildOptionItem(SELECTION_KEY_ONE,
-                    ['synonym of KEY_ONE 1', 'synonym of KEY_ONE 2'])
-                    .setTitle('Number one'),
-                app.buildOptionItem(SELECTION_KEY_TWO,
-                    ['synonym of KEY_TWO 1', 'synonym of KEY_TWO 2'])
-                    .setTitle('Number two'),
-            ])
+        res.json(body);
+        // app.buildCarousel()
+        //     .addItems([
+        //         app.buildOptionItem(SELECTION_KEY_ONE,
+        //             ['synonym of KEY_ONE 1', 'synonym of KEY_ONE 2'])
+        //             .setTitle('Number one'),
+        //         app.buildOptionItem(SELECTION_KEY_TWO,
+        //             ['synonym of KEY_TWO 1', 'synonym of KEY_TWO 2'])
+        //             .setTitle('Number two'),
+        //     ])
     });
     // return res.json({
     //     speech: speech,
